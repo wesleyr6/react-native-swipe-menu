0d26e7c38b2912081a1cf90c10d5fda52e8d30e4
Wesley Amaro <contato@wesleyamaro.com>
2018-08-31 14:12:09 -0300

    feat: calling statusBar component

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { View } from 'react-native';
+import StatusBar from './src/components/StatusBar';
 import Content from './src/components/Content';
 import Sidebar from './src/components/Sidebar';
 
 const App = () => {
 	return (
-		<Sidebar contentComponent={<Content />} />
+		<View>
+			<StatusBar bg={'#313131'} />
+			<Sidebar contentComponent={<Content />} />
+		</View>
 	);
 }
 
