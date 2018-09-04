9067a2f4a9064641de4278828000acb296371feb
Wesley Amaro <contato@wesleyamaro.com>
2018-09-04 14:40:18 -0300

    feat: calling menu component

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { View } from 'react-native';
 import StatusBar from './src/components/StatusBar';
 import Content from './src/components/Content';
 import Sidebar from './src/components/Sidebar';
+import Menu from './src/components/Menu';
 
 const App = () => {
 	return (
 		<View>
 			<StatusBar bg={'#313131'} />
 			
-			<Sidebar>
+			<Sidebar menu={<Menu />}>
 				<Content />
 			</Sidebar>
 		</View>
